7f41d8677c5c805488dd306d3141d31da0465ca4
Yash Goyal <yashgoyalg400@gmail.com>
2021-01-15 23:03:20 +0530

    updated3

--- disease_prediction/__init__.py
+++ disease_prediction/__init__.py
@@ -16,9 +16,10 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('DB_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('DB_PASS')
 
