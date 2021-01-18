501a00102b77940ccf1a6977fc25955a69b1e1f0
Yash Goyal <yashgoyalg400@gmail.com>
2021-01-18 17:24:33 +0530

    updating databse

--- disease_prediction/models.py
+++ disease_prediction/models.py
@@ -8,6 +8,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
